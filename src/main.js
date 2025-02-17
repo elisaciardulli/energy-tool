b6bdfc11330e35987a4d6f4cad1e2345e71c24dc
Elisa Ciardulli <e.ciardulli@noi.bz.it>
2025-02-17 14:23:08 +0100

    removed vuetify v-calendar

--- src/main.js
+++ src/main.js
@@ -6,8 +6,6 @@ import '@mdi/font/css/materialdesignicons.css';
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-import { VCalendar } from 'vuetify/labs/VCalendar'
-/* import { it } from 'vuetify/locale' */
 
 // Components
 import App from './App.vue'
@@ -15,15 +13,11 @@ import App from './App.vue'
 const vuetify = createVuetify({
   directives,
   components: {
-    VCalendar,
     ...components
   },
   icons: {
     iconfont: 'mdi',
-  },/* 
-  locale: {
-    current: 'it',
-  }, */
+  },
 })
 
 createApp(App).use(vuetify).mount('#app')
