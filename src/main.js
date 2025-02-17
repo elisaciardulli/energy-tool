0adb2c8b655af094f29756c5e2bec7f4f251217f
Elisa Ciardulli <e.ciardulli@noi.bz.it>
2025-02-17 14:29:05 +0100

    installed vue-cal

--- src/main.js
+++ src/main.js
@@ -7,6 +7,10 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+// Vue-cal
+import VueCal from 'vue-cal'
+import 'vue-cal/dist/vuecal.css'
+
 // Components
 import App from './App.vue'
 
