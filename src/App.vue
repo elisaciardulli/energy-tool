6ab6abbf2bb2273e7c874f2b7c43c0bf0ebfabc8
Elisa Ciardulli <e.ciardulli@noi.bz.it>
2025-02-17 15:44:40 +0100

    added event examples, no real time data

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <v-container fluid>
     <v-row>
       <!-- Small Calendar - Date Picker -->
-      <v-col cols="12" md="3"> <!-- Adjusted to 3 on medium screens -->
+      <v-col cols="12" md="3">
         <div class="small-cal-container">
           <vue-cal
             class="vuecal--date-picker vuecal--blue-theme"
@@ -10,19 +10,25 @@
             hide-view-selector
             :time="false"
             :transitions="false"
-            active-view="month"
+            active-view="week"
             :disable-views="['week']"
+            today-button
             style="width: 100%; height: auto;">
           </vue-cal>
         </div>
       </v-col>
 
       <!-- Big Calendar -->
-      <v-col cols="12" md="9"> <!-- Adjusted to 9 on medium screens -->
+      <v-col cols="12" md="9">
         <div class="big-cal-container">
           <vue-cal 
             class="vuecal--blue-theme" style="height: 80%" 
-            :disable-views="['years']">
+            :disable-views="['years']"
+            :time-from="6 * 60"
+            :time-to="21 * 60"
+            :time-step="30"
+            today-button
+            :special-hours="specialHours">
           </vue-cal>
         </div>
       </v-col>
@@ -31,10 +37,95 @@
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      specialHours: {
+        // Monday (1)
+        1: {
+          from: 9 * 60,
+          to: 17 * 60,
+          class: 'heating',
+          label: 'Heating'
+        },
+        
+        // Tuesday (2)
+        2: {
+          from: 9 * 60,
+          to: 17 * 60,
+          class: 'heating',
+          label: 'Heating'
+        },
+
+        // Wednesday (3) - Multiple shifts
+        3: [
+          {
+            from: 9 * 60,
+            to: 17 * 60,
+            class: 'heating',
+            label: 'Heating'
+          }
+        ],
+
+        // Thursday (4)
+        4: {
+          from: 9 * 60,
+          to: 17 * 60,
+          class: 'heating',
+          label: 'Heating'
+        },
+
+        // Friday (5)
+        5: {
+          from: 9 * 60,
+          to: 17 * 60,
+          class: 'heating',
+          label: 'Heating'
+        },
+
+        // Saturday (6)
+        6: {
+          from: 9 * 60,
+          to: 17 * 60,
+          class: 'heating',
+          label: 'Heating'
+        },
+
+        // Sunday (7)
+        7: {
+          from: 9 * 60,
+          to: 17 * 60,
+          class: 'heating',
+          label: 'Heating'
+        }
+      }
+    }
+  }
+}
 </script>
 
-<style scoped>
+<style>
 v-container {
   max-width:none !important;
+}
+
+/* Style events */
+.vuecal__special-hours {
+  display: flex;
+  padding-block: 0.7rem;
+  padding-inline-start: 0.5rem;
+}
+
+/* Style heating events */
+.heating {
+  background-color: rgb(225, 170, 170);
+  color: rgb(57, 57, 57);
+  border-left: 4px solid rgb(228, 102, 102);
+  box-shadow: 0 4px 8px rgba(227, 86, 86, 0.4);
+  border-radius: 4px;
+}
+
+.vuecal__now-line {
+  color: black;
 }
 </style>
