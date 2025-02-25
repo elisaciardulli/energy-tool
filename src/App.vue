6ad5900cbbf1f415a206d627672f6fd8dd08cabf
Elisa Ciardulli <e.ciardulli@noi.bz.it>
2025-02-25 10:18:03 +0100

    events are loaded correctly on mounted

--- src/App.vue
+++ src/App.vue
@@ -33,6 +33,7 @@
             :selected-date="selectedDate"
             :events="events"
             @view-change="onViewChange"
+            :on-event-click="onEventClick"
             style="height: 90vh"
             >
             <template #split-label="{ split }">
@@ -49,6 +50,17 @@
               </v-tooltip>
             </template>
           </vue-cal>
+          <v-dialog v-model="showDialog">
+            <!--Dialog window-->
+            <v-card>
+              <v-card-title>
+                <span>{{ selectedEvent.title }}</span>
+              </v-card-title>
+              <v-card-text>
+                <p>This is where the table will go</p>
+              </v-card-text>
+            </v-card>
+          </v-dialog>
         </div>
       </v-col>
     </v-row>
@@ -59,79 +71,105 @@
 export default {
   data() {
     return {
+      selectedEvent: {},
+      showDialog: false,
       currDate: new Date(),
       selectedDate: new Date(),
       daySplits: [
-        {label: 'Seminar 1', class: 'seminar1' },
-        {label: 'Seminar 2', class: 'seminar2' },
-        {label: 'Seminar 3', class: 'seminar3' },
-        {label: 'Seminar 4', class: 'seminar4' },
-        {label: 'Seminar Area', class: 'seminarArea' },
-        {label: 'Foyer', class: 'foyer' },
-        {label: 'Crane Hall', class: 'cranehall' }
+        {id:"Seminar 1", label: 'Seminar 1', class: 'seminar1' },
+        {id:"Seminar 2", label:'Seminar 2', class: 'seminar2' },
+        {id:"Seminar 3", label: 'Seminar 3', class: 'seminar3' },
+        {id:"Seminar 4", label: 'Seminar 4', class: 'seminar4' },
+        {id:"Seminar Area", label: 'Seminar Area', class: 'seminarArea' },
+        {id:"Foyer", label: 'Foyer', class: 'foyer' },
+        {id:"Crane Hall", label: 'Crane Hall', class: 'cranehall' }
       ],
       events: []
     };
   },
   async mounted() {
-    // Fetch data when the component is mounted
-    const startDate = new Date(this.selectedDate);
-    startDate.setUTCHours(0,0,0,0);
-    const endDate = new Date(this.selectedDate);
-    endDate.setUTCHours(23, 59, 59, 999);
-
-    const fetchedEvents = await this.getData(startDate, endDate);
-    if (fetchedEvents && fetchedEvents.Items) {
-      console.log("fetched events: ", fetchedEvents + " fetchedEventsItem: ", fetchedEvents.Items);
-      const eventsForCalendar = fetchedEvents.Items.map(event => {
-
-        return {
-          start: this.getDateFormatted(event.RoomBooked[0].StartDate),
-          end: this.getDateFormatted(event.RoomBooked[0].EndDate),
-          title: event.EventTitle.it,
-          content: "",
-          class: "event",
-          split: event.RoomBooked[0].Space,
-        };
-      });
-      this.events = eventsForCalendar;
-      console.log("events: ", this.events);
-    }
+    this.loadEvents()
   },
   methods: {
+    async loadEvents() {
+      //Make sure the events list is empty
+      this.events = [];
+      // Fetch data when the component is mounted
+      console.log("selectedDate: ", this.selectedDate);
+      const startDate = new Date(this.selectedDate);
+      startDate.setHours(0,0,0,0);
+      console.log("startDate: ", startDate);
+      const endDate = new Date(this.selectedDate);
+      endDate.setHours(23, 59, 59, 999);
+      console.log("endDate: ", endDate);
+
+      const fetchedEvents = await this.getData(startDate, endDate);
+      if (fetchedEvents && fetchedEvents.Items) {
+        const eventsForCalendar = fetchedEvents.Items.map(event => {
+          for(const i = 0; i <= event.RoomBooked.length; i++) {
+            return {
+            start: this.getDateFormatted(event.RoomBooked[i].StartDate),
+            end: this.getDateFormatted(event.RoomBooked[i].EndDate),
+            title: event.EventTitle.it,
+            content: "",
+            class: "event",
+            split: this.mapStatus(event.RoomBooked[i].SpaceDesc),
+            };
+          }
+        });
+        this.events = eventsForCalendar;
+        console.log("events: ", this.events);
+      } 
+    },
+    mapStatus(string) {
+      const roomList = [
+        "Seminar 1",
+        "Seminar 2",
+        "Seminar 3",
+        "Seminar 4",
+        "Seminar Area",
+        "Foyer",
+        "Crane Hall",
+      ];
+      for (let i = 0; i < roomList.length; i++) {
+        let roomRegex = new RegExp(".*" + roomList[i] + ".*", "i");
+        if (roomRegex.test(string)) {
+          return roomList[i];
+        }
+      }
+      return "NO MATCH";
+    },
     getDateFormatted(dateString) {
       const date = new Date(dateString);
       
-      const year = date.getUTCFullYear();
-      const month = String(date.getUTCMonth() + 1).padStart(2, "0");
-      const day = String(date.getUTCDate()).padStart(2, "0");
-      const hours = String(date.getUTCHours()).padStart(2, "0");
-      const minutes = String(date.getUTCMinutes()).padStart(2, "0");
+      const year = date.getFullYear();
+      const month = String(date.getMonth() + 1).padStart(2, "0");
+      const day = String(date.getDate()).padStart(2, "0");
+      const hours = String(date.getHours()).padStart(2, "0");
+      const minutes = String(date.getMinutes()).padStart(2, "0");
 
       return `${year}-${month}-${day} ${hours}:${minutes}`;
     },
+    onEventClick(event, e) {
+      this.selectedEvent;
+      this.showDialog = true;
 
-    async onViewChange(newDate) {
-      this.selectedDate = newDate.startDate;
-      
-      const startDate = new Date(this.selectedDateDate);
-      startDate.setHours(0,0,0,0);
-      const endDate = new Date(this.selectedDate);
-      endDate.setHours(23, 59, 59, 999);
+      // Prevent navigating to narrower view (default vue-cal behavior).
+      e.stopPropagation()
+    },
 
-      // fecth data on view change
-      const fetchedEvents = await this.getData(startDate.toISOString(), endDate.toISOString());
-      if (fetchedEvents && fetchedEvents.Items) {
-        this.events = fetchedEvents.Items; // Update the events array with fetched data
-      }
+    async onViewChange() {
+      this.loadEvents();
     },
     async getData(startDate, endDate) {
       const baseUrl = "https://tourism.api.opendatahub.com/v1/EventShort";
+      console.log("start date in getDate: ", this.getDateFormatted(startDate));
+      console.log("end date in getDate: ", this.getDateFormatted(endDate));
 
       const params = new URLSearchParams({
         pagenumber: "1",
-        startdate: startDate.toISOString().split('T')[0],
-        enddate: endDate.toISOString().split('T')[0],
+        startdate: this.getDateFormatted(startDate),
+        enddate: this.getDateFormatted(endDate),
         eventlocation: "NOI",
         active: "true",
         sortorder: "ASC",
@@ -166,39 +204,38 @@ v-container {
 
 /* Style events */
 .vuecal__event-title {
-  font-size: 1.2rem;
-  font-weight: bold;
-  padding-top: 0.5rem;
+  font-size: 1rem;
+  font-weight: 100;
+  padding: 0.5rem;
 }
-
+.event {
+  background-color: rgb(200, 230, 250);
+  color: rgb(57, 57, 57);
+  border-left: 4px solid rgb(110, 170, 215);
+}
 .vuecal__event-time {
   display: none;
 }
 
-.vuecal__event-content {
+.vuecal__no-event {
+  display: none;
 }
 
 /* Style heating events */
-.event {
-  background-color: rgb(225, 170, 170);
+.heating {
+  background-color: rgb(240, 190, 190);
   color: rgb(57, 57, 57);
-  border-left: 4px solid rgb(228, 102, 102);
-  box-shadow: 0 4px 8px rgba(227, 86, 86, 0.4);
+  border-left: 4px solid rgb(230, 100, 100);
+  box-shadow: 0 4px 8px rgba(230, 80, 80);
   border-radius: 4px;
 }
 
 .vuecal__now-line {
-  color: black;
+  color: rgb(63, 63, 63);
 }
 
 /*Style split columns*/
 .vuecal .day-split-header {
   font-size: 0.8rem;
-}
-.vuecal__body {
-}
-
-.vuecal__no-event {
-  display: none;
 }
 </style>
